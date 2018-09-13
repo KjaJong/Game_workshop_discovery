99f379dda50cb9518b87cfd5b1c1f8f3f60d342e
Menno de Jong <menno.dejong@ziggo.nl>
2018-09-13 16:11:16 +0200

    Copied and pasted the player controls.
    And they work

---
+++ Discovery_ball/Assets/Scripts/CameraController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    [SerializeField][Tooltip("The object the camera should follow.")]
+    private GameObject playerObject = null;
+
+    private Vector3 offsetFromPlayer;
+
+	// Use this for initialization
+	void Start()
+    {
+        offsetFromPlayer = transform.position - playerObject.transform.position;
+	}
+	
+	/// <summary>
+    /// Update that controls the camera position, called after all physics have been done
+    /// </summary>
+	void LateUpdate()
+    {
+        transform.position = playerObject.transform.position + offsetFromPlayer;
+	}
+}
---
+++ Discovery_ball/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    [SerializeField][Tooltip("The object we apply forces to to move.")]
+    private Rigidbody playerBody = null;
+    [SerializeField][Tooltip("The speed of the ball.")]
+    private float movementSpeed;
+    [SerializeField][Tooltip("The height the ball jumps.")]
+    private float jumpHeight;
+	
+	// Update is called once per frame
+	void FixedUpdate()
+    {
+		if(playerBody == null) { return; }
+
+        //May include jumping in the future, for now only moves on the plane
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+        playerBody.AddForce(movement * movementSpeed);
+	}
+}
